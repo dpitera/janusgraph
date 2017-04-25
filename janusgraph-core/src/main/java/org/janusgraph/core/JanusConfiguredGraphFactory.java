a0cbe35bbf6e4771e31bef7ee3315d6aff21354d
David Pitera <dpitera@us.ibm.com>
2017-04-24 20:13:04 -0400

    Create ConfigurationManagementGraph and factory In this commit, we create a ConfigurationManagementGraph singleton instantiated at server start by the JanusGraphManager, should the user supply a graph with the key "JanusConfigurationGraph" inside their YAML's "graphs" objects. The file supplied with this key will define the configuration used to open the ConfigurationGraph used by the ConfigurationManagementGraph.
    The ConfigurationManagementGraph allows users to create:
    1. A configuration per graph (the supplied MapConfiguration must include
    the property "Graph_Name")
    2. A Template_Configuration (the supplied MapConfiguration must not
    include the property "Graph_Name").
    
    Thus, by doing so, user's can access their graphs using the
    JanusConfiguredGraphFactory and reference their graph's by a string
    representation of the graphName.
    
    If using (1) above, then a user must first create the configuration
    using the ConfigurationManagementGraph.createConfiguration API, and then
    the user can access the graph by using the
    JanusConfiguredGraphFactory.open(String graphName).
    
    If using (2) above, the user must first create the template
    configuration using the
    ConfigurationManagementGraph.createTemplateConfiguation API, then create
    the graph with a supplied String graphName using
    JanusConfiguredGraphFactory.create(String graphName) which will return
    the graph reference. From there on out, the user can simply use the
    JanusConfiguredGraphFactory.open(String graphName) to access their
    graph.
    
    The ConfigurationManagementGraph also includes updates and remove APIs.
    
    Please note: whenever updating a graph's configuration using the
    ConfigurationManagementGraph API, the change is only guaranteed to take
    effect when the graph in question has been closed and reopened on every
    Janus Graph node in your cluster. If a graph was created using the
    Template_Configurtion and you update the Template_Configuration, then
    any graph you wish to open using the new configuration must: 1. be
    closed on all JanusGraph nodes 2. remove its configuration stored in the
    ConfigurationManagementGraph and 3. recreate its configuration in the
    ConfigurationManagementGraph.
    
    Signed-off-by: David Pitera <dpitera@us.ibm.com>

---
+++ janusgraph-core/src/main/java/org/janusgraph/core/JanusConfiguredGraphFactory.java
@@ -0,0 +1,146 @@
+// Copyright 2017 JanusGraph Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.janusgraph.core;
+
+import org.janusgraph.graphdb.management.ConfigurationManagementGraph;
+import org.janusgraph.graphdb.management.JanusGraphManager;
+import org.janusgraph.graphdb.database.StandardJanusGraph;
+import org.janusgraph.graphdb.configuration.GraphDatabaseConfiguration;
+import org.janusgraph.diskstorage.configuration.backend.CommonsConfiguration;
+import org.janusgraph.diskstorage.configuration.ConfigOption;
+import org.janusgraph.diskstorage.BackendException;
+import org.janusgraph.graphdb.management.utils.ConfigurationManagementGraphNotEnabledException;
+import org.janusgraph.diskstorage.StandardStoreManager;
+
+import org.apache.commons.configuration.MapConfiguration;
+
+import java.util.Map;
+import java.util.List;
+import java.util.Properties;
+import java.util.HashMap;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+
+/**
+ * This class allows for static methods to: 1) create graph references denoted by a {@link String}
+ * graphName using a previously created template configuration using the
+ * {@link ConfigurationManagementGraph} or 2) open a graph references denoted by a {@link String}
+ * graphName for graphs that have been previously created or for graphs that have corresponding
+ * previously created configurations created using the {@link ConfigurationManagementGraph}; this
+ * class also defines a close which allows for removal of these {@link Graph} objects from the
+ * {@link JanusGraphManager} reference tracker.
+ */
+public class JanusConfiguredGraphFactory {
+    /**
+     * Creates a {@link JanusGraph} configuration stored in {@ConfigurationGraphManagament}
+     * configurationGraph and a {@link JanusGraph} graph reference according to the single
+     * Template Configuration  previously created by the {@link ConfigurationManagementGraph} API;
+     * if a configuration exists for this graph already, or a Template Configuration does not exist,
+     * we throw a {@link RuntimeException}; If the Template Configuration does not include its
+     * backend's respective keyspace/table/storage_directory parameter, we set the keyspace/table
+     * to the {@link String} supplied graphName or we append the graphName to the supplied
+     * storage_root parameter.
+     *
+     * @param String graphName
+     *
+     * @return JanusGraph
+     */
+    public static synchronized JanusGraph create(final String graphName) {
+        ConfigurationManagementGraph configGraphManagement = getConfigGraphManagementInstance();
+
+        Map<String, Object> graphConfigMap = configGraphManagement.getConfiguration(graphName);
+        if (null != graphConfigMap) throw new RuntimeException(String.format("Configuration for graph %s already exists.", graphName));
+        Map<String, Object> templateConfigMap = configGraphManagement.getTemplateConfiguration();
+        if (null == templateConfigMap) {
+            throw new RuntimeException("Please create a template Configuration using the ConfigurationManagementGraph#createTemplateConfiguration API.");
+        }
+
+        templateConfigMap.put(ConfigurationManagementGraph.PROPERTY_GRAPH_NAME, graphName);
+
+        JanusGraph g;
+        try {
+            g = (JanusGraph) JanusGraphManager.getInstance().openGraph(graphName, (String gName) -> {
+                return new StandardJanusGraph(new GraphDatabaseConfiguration(new CommonsConfiguration(new MapConfiguration(templateConfigMap))));
+            });
+        } catch (Exception e) {
+            configGraphManagement.removeConfiguration(graphName);
+            throw new RuntimeException(e);
+        }
+        configGraphManagement.createConfiguration(new MapConfiguration(templateConfigMap));
+        return g;
+    }
+
+    /**
+     * Open a {@link JanusGraph} using a previously created Configuration using the
+     * {@link ConfigurationManagementGraph} API; if a corresponding configuration does not exist, we throw
+     * a {@link RuntimeException}, else return the Graph;
+     *
+     * NOTE: If your configuration corresponding to this graph does not contain information about
+     * the backend's keyspace/table/storage directory, then we set the keyspace/table to the
+     * graphName or set the storage directory to the storage_root + /graphName.
+     */
+    public static JanusGraph open(String graphName) {
+        ConfigurationManagementGraph configGraphManagement = getConfigGraphManagementInstance();
+        Map<String, Object> graphConfigMap = configGraphManagement.getConfiguration(graphName);
+        if (null == graphConfigMap) {
+            throw new RuntimeException("Please create configuration for this graph using the ConfigurationManagementGraph#createConfiguration API.");
+        }
+        return (JanusGraph) JanusGraphManager.getInstance().openGraph(graphName, (String gName) -> {
+            return new StandardJanusGraph(new GraphDatabaseConfiguration(new CommonsConfiguration(new MapConfiguration(graphConfigMap))));
+        });
+    }
+
+    /**
+     * Removes the graph corresponding to the supplied {@link String} graphName
+     * from the {@link JanusGraphManager} {@link Map<String, Graph} graph reference tracker and
+     * returns the corresponding Graph, or null if it doesn't exist.
+     *
+     * @param configuration Graph
+     * @return JanusGraph
+     */
+    public static JanusGraph close(String graphName) {
+        return (JanusGraph) JanusGraphManager.getInstance().removeGraph(graphName);
+    }
+
+    /**
+     * Returns a {@link MapConfiguration} corresponding to supplied .properties file
+     * which lives at the supplied {@link String} fileLocation.
+     *
+     * @param {@link String} properties file location
+     *
+     * @return MapConfiguration
+     */
+    public static MapConfiguration fileToMapConfiguration(final String fileLocation) throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(new File(fileLocation)));
+        Map<String, Object> map = new HashMap<String, Object>();
+        properties.entrySet().stream().forEach(entry -> {
+            map.put((String) entry.getKey(), entry.getValue());
+        });
+        return new MapConfiguration(map);
+    }
+
+    private static ConfigurationManagementGraph getConfigGraphManagementInstance() {
+        ConfigurationManagementGraph configGraphManagement = null;
+        try {
+            configGraphManagement = ConfigurationManagementGraph.getInstance();
+        } catch (ConfigurationManagementGraphNotEnabledException e) {
+            throw new RuntimeException(e);
+        }
+        return configGraphManagement;
+    }
+}
+
